55a57692b1b46ac384073842543bc6b14f33e1f4
Serdar Cevher <scevher@gmail.com>
2023-07-25 01:16:37 +0300

    Initial commit

---
+++ content.js
@@ -0,0 +1,34 @@
+// content.js
+
+const twitterFaviconBase64 = "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAJkSURBVHgB7VZBbtpQEH3zIW0WVYuXVaH4Bs0NSk4AOUFhEarskhMknIDsqkKlcIT0BNAT1D1B3ZJK3dmVuirwp/MhVmzAxiagKBJv9+ePZ97M/JkxsMMODwzChlD84FWQp3MxeCDHAhiumB+MJrr1+8Ryw3p/9+H4DctfIPCq49Xlw8Kv99YlMuB19885gy/i7llziwGfFFWJyR02XzSCuwiBUse7BlFVaz5LS8KQVkRXaXRJsqImfDjKSZBNyzEyFWFKVJ4KFbWLElUao6KbSk8i9TXgTPaorxTskPwOxa7/9baGt4zg8oQbNyfWYJlRU0/KUx9ZwNwYNq1ecFRzl18QpW0bB0Ks//KjV1uwlbuLJA3GxEdh5wb5yGEPl3qMd2xecYQHKnlFlVLX95kxYCFKGg5IlU2a0uLpCM68LEJA+sJ/Dm6Jy3aMjQIRakRUm+UuvfOp/X34iQSejeFo0Hdx4optG5uFH/R+GHNvANcm3VtwLs+Lvy2TRwhIOnrYHhysIuDKcCDwGbYAjglOzQt+HssElF6dvoNNOZeuCSbfSgIGMjILMo4/ExZf7TqghNLmlwm1gpSC2tmaLAZMvWGz0Iu7XpqBm2NrQNN5cD+Y5ZOTdZyok3RZMusZOJUN+QZrQFb0oQkG6xIIYHe8A03Unx/Ryd6jS2ctAsbxmFRVynGKlM5na5ePVkUe0p+h9MmraS2zXqYgmSWjOPtElHbLTVB3Q79gqQlMScxqXpeav0UWiGMmXKSNOpZAAPvKs/U/1MRoxRxl+5WD+psUy2D5IdmRVoWjnqDnLlkyO+zwaPAf1zXwZL751PUAAAAASUVORK5CYII="; // Replace this with the actual base64 string
+
+function applyChanges() {
+	setTimeout(() => {
+		//let placeholder = document.querySelector('div#placeholder');
+		//placeholder.innerHTML = "<div id='placeholder_logo'></div>";
+
+		let heading = document.querySelector('h1[role="heading"] a div');
+		if (heading) {
+			heading.innerHTML = '&nbsp;';
+		}
+
+		setFaviconToIconFile();
+	}, 100)
+}
+
+function setFaviconToIconFile() {
+	const existingFavicon = document.querySelector("link[rel='shortcut icon']");
+
+	if (existingFavicon) {
+		existingFavicon.remove();
+	}
+
+	const faviconLink = document.createElement("link");
+	faviconLink.rel = "icon";
+	faviconLink.href = twitterFaviconBase64;
+
+	document.head.appendChild(faviconLink);
+}
+
+applyChanges();
+
